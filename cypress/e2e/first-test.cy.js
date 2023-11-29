fa1417df6dad877b88b0f8986035dfe3f350e3da
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-30 00:37:02 +0530

    Add: More tests

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -1,5 +1,10 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Loading and validating test", () => {
+  it("opens the page correctly", () => {
     cy.visit("http://localhost:5173");
+  });
+
+  it("validates the 6 elements are there in the page", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("li").should("have.length", 6);
   });
 });
